7cf2419994095d535a86260bfc97929810bd895f
alepistola <pistola.alessandro@virgilio.it>
2020-05-07 01:36:32 +0200

    Upload - Started implementing XmpFile class

---
+++ TimelapseEditor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TimelapseEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* XmpFile test */
+            string exposure;
+            string imagePath = "C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus - 5 - D\\DSC_0003.NEF";
+            XmpFile file = new XmpFile(imagePath);
+            exposure = file.ReadTag("Exposure2012").ToString();
+            Console.WriteLine($"[!] Retrieved Exposure2012 from {file.GetPath()}, value:{exposure}");
+        }
+    }
+}

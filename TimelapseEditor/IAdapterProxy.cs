ea93eb6b346d176c0d695962f36135647bb47091
alepistola <pistola.alessandro@virgilio.it>
2020-05-08 01:24:56 +0200

    Added interface for Proxy

---
+++ TimelapseEditor/IAdapterProxy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimelapseEditor
+{
+    interface IAdapterProxy
+    {
+        public void SetExposure(double value);
+        public double GetExposure();
+        public void ApplyPreset();
+        public string GetImagePath();
+    }
+}

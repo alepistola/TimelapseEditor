14c7b6fe5b5916421792bb1ca885fa4be1be6cf7
alepistola <pistola.alessandro@virgilio.it>
2020-05-07 15:54:53 +0200

    Added test for saving a key

--- TimelapseEditor/Program.cs
+++ TimelapseEditor/Program.cs
@@ -7,11 +7,14 @@ namespace TimelapseEditor
         static void Main(string[] args)
         {
             /* XmpFile test */
-            string exposure;
+            string exposure, contrast;
             string imagePath = "C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus - 5 - D\\DSC_0003.NEF";
             XmpFile file = new XmpFile(imagePath);
             exposure = file.ReadTag("Exposure2012").ToString();
             Console.WriteLine($"[!] Retrieved Exposure2012 from {file.GetPath()}, value:{exposure}");
+            file.SaveTag("Contrast2012", "+1.00");
+            contrast = file.ReadTag("Contrast2012").ToString();
+            Console.WriteLine($"[!] Retrieved Contrast2012 from {file.GetPath()}, value:{contrast}");
         }
     }
 }

4361d6e69d73403a3b39c4da5c89400d79c72076
alepistola <pistola.alessandro@virgilio.it>
2020-05-07 17:18:33 +0200

    Implemented interface for adapters

---
+++ TimelapseEditor/IPhotoChanges.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimelapseEditor
+{
+    interface IPhotoChanges
+    {
+        public void SetExposureToFile(double value);
+        public double GetExposureFromFile();
+        public void ApplyPresetToFile(Preset preset);
+        public string GetImagePath();
+        public string GetFilePath();
+    }
+}

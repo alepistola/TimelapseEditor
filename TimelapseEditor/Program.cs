fac6a2a84bae39c6537820e9bc66410a0b8f2fe5
alepistola <pistola.alessandro@virgilio.it>
2020-05-07 17:19:53 +0200

    Added test for cameraRaw adaprte

--- TimelapseEditor/Program.cs
+++ TimelapseEditor/Program.cs
@@ -6,15 +6,23 @@ namespace TimelapseEditor
     {
         static void Main(string[] args)
         {
-            /* XmpFile test */
-            string exposure, contrast;
+            double exposure, contrast;
             string imagePath = "C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus - 5 - D\\DSC_0003.NEF";
+
+            /* XmpFile test
             XmpFile file = new XmpFile(imagePath);
-            exposure = file.ReadTag("Exposure2012").ToString();
+            exposure = file.ReadTag("crs:Exposure2012").ToString();
             Console.WriteLine($"[!] Retrieved Exposure2012 from {file.GetPath()}, value:{exposure}");
-            file.SaveTag("Contrast2012", "+1.00");
-            contrast = file.ReadTag("Contrast2012").ToString();
+            file.SaveTag("crs:Contrast2012", "+1.00");
+            contrast = file.ReadTag("crs:Contrast2012").ToString();
             Console.WriteLine($"[!] Retrieved Contrast2012 from {file.GetPath()}, value:{contrast}");
+            */
+
+            /* Camera raw adapter */
+            CameraRawXmpAdapter adapter = new CameraRawXmpAdapter(imagePath);
+            exposure = adapter.GetExposureFromFile();
+            Console.WriteLine($"[!] Retrieved Exposure2012 from {adapter.GetFilePath()}, value:{exposure}");
+
         }
     }
 }

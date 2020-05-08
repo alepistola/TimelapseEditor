95d27128b38996a2caf1231fc554dc42cef6ffca
alepistola <pistola.alessandro@virgilio.it>
2020-05-08 17:17:13 +0200

    GetExposureFromFile now can return null value

--- TimelapseEditor/IPhotoChanges.cs
+++ TimelapseEditor/IPhotoChanges.cs
@@ -7,7 +7,7 @@ namespace TimelapseEditor
     interface IPhotoChanges
     {
         public void SetExposureToFile(double value);
-        public double GetExposureFromFile();
+        public double? GetExposureFromFile();
         public void ApplyPresetToFile(Preset preset);
         public string GetImagePath();
         public string GetFilePath();

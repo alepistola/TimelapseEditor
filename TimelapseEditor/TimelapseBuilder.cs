3c07d5dfa9c4e81f7938c3fc67bc4a7f3ec12b2f
alepistola <pistola.alessandro@virgilio.it>
2020-05-13 00:59:36 +0200

    Added class: implementation of interface

---
+++ TimelapseEditor/TimelapseBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimelapseEditor
+{
+    public class TimelapseBuilder : ITimelapseBuilder
+    {
+        private Timelapse _timelapse;
+
+        public TimelapseBuilder(string firstImagePath)
+        {
+            _timelapse = Timelapse.Instance(firstImagePath);
+            this.Reset();
+        }
+
+        private void Reset()
+        {
+            _timelapse.RemoveChanges();
+        }
+
+        public void AnalyzeExposureTime() { _timelapse.AnalyzeExposure(); }
+        public void AddPreset(Preset preset) { _timelapse.ApplyPreset(preset); }
+        public void AddVignetting(int intensity) { _timelapse.AddVignetting(intensity); }
+        public Timelapse GetTimelapse() => _timelapse;
+    }
+}

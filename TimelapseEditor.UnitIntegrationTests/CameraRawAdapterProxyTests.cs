060786b8a7b4bdc4f46dd20dfd1031552fc53ed4
alepistola <pistola.alessandro@virgilio.it>
2020-05-13 01:07:22 +0200

    Added project for unit tests & integration tests

---
+++ TimelapseEditor.UnitIntegrationTests/CameraRawAdapterProxyTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TimelapseEditor.UnitIntegrationTests
+{
+    [TestClass]
+    public class CameraRawAdapterProxyTests
+    {
+        private CameraRawAdapterProxy proxy;
+
+        private void Setup()
+        {
+            // Arrange
+            string firstImagePath = "C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus - 5 - D\\DSC_0004.NEF";
+            if (File.Exists("C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus -5 - D\\DSC_0004.xmp"))
+            {
+                File.Delete("C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus -5 - D\\DSC_0004.xmp");
+            }
+            proxy = new CameraRawAdapterProxy(firstImagePath);
+        }
+
+        [TestMethod]
+        public void SetNGetExposure_ReturnsExposure()
+        {
+            Setup();
+
+            proxy.SetExposure(2.56);
+            double exp = proxy.GetExposure();
+
+            Assert.IsTrue(exp == 2.56);
+        }
+
+        [TestMethod]
+        public void SaveExposure_ReturnsVoid()
+        {
+            Setup();
+
+            proxy.SetExposure(2.01);
+            proxy.SaveExposure();
+
+            CameraRawXmpAdapter adap = new CameraRawXmpAdapter("C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus - 5 - D\\DSC_0004.NEF");
+            double exp = adap.GetExposureFromFile().Value;
+            Assert.IsTrue(exp == 2.01);
+        }
+    }
+}
---
+++ TimelapseEditor.UnitIntegrationTests/CameraRawXmpAdapterTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TimelapseEditor.UnitIntegrationTests
+{
+    [TestClass]
+    public class CameraRawXmpAdapterTests
+    {
+        private CameraRawXmpAdapter adapter;
+
+        private void Setup()
+        {
+            // Arrange
+            string firstImagePath = "C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus - 5 - D\\DSC_0004.NEF";
+            if (File.Exists("C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus -5 - D\\DSC_0004.xmp"))
+            {
+                File.Delete("C:\\Users\\Alessandro\\Pictures\\viaggio Erasmus -5 - D\\DSC_0004.xmp");
+            }
+            adapter = new CameraRawXmpAdapter(firstImagePath);
+        }
+
+        [TestMethod]
+        public void SetNGetExposureFromToFileTest_ReturnsDouble()
+        {
+            Setup();
+
+            adapter.SetExposureToFile(1.25);
+            double? exp = adapter.GetExposureFromFile();
+            Assert.IsTrue(exp.Value == 1.25);
+        }
+
+        [TestMethod]
+        public void GetExifFromPhotoTest_ReturnsDictionary()
+        {
+            Setup();
+
+            var exif = adapter.GetExifFromPhoto();
+            Assert.IsTrue(exif.Count == 3);
+        }
+
+        [TestMethod]
+        public void GetImageFilenameTest_ReturnsFirstImagePath()
+        {
+            Setup();
+
+            string imageName = adapter.GetImageFileName();
+            Assert.IsTrue(imageName == "DSC_0004.NEF");
+        }
+    }
+}

adcb28fe1c2fd51e2a6cd914b507d7aac9d7ad09
alepistola <pistola.alessandro@virgilio.it>
2020-05-13 01:03:00 +0200

    Added TimelapseBuilder interface

---
+++ TimelapseEditor/ITimelapseBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimelapseEditor
+{
+    public interface ITimelapseBuilder
+    {
+        void AnalyzeExposureTime();
+        void AddPreset(Preset preset);
+        void AddVignetting(int intensity);
+        Timelapse GetTimelapse();
+    }
+}

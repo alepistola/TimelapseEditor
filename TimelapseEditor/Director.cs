dc74acf125ad6e66523540228d8bc4da66e68041
alepistola <pistola.alessandro@virgilio.it>
2020-05-13 01:04:58 +0200

    Added class for manage builder object

---
+++ TimelapseEditor/Director.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimelapseEditor
+{
+    public class Director
+    {
+        private ITimelapseBuilder _builder;
+        public ITimelapseBuilder SetBuilder(ITimelapseBuilder builder)
+        {
+            _builder = builder;
+            return _builder;
+        }
+
+        public void BuildStabilizedTimelapse() { _builder.AnalyzeExposureTime(); }
+        public void BuildStabilizedWithPresetTimelapse(Preset preset) 
+        {
+            BuildStabilizedTimelapse();
+            _builder.AddPreset(preset);
+        }
+        public void BuildStabilizedWithVignetteTimelapse(int intensity)
+        {
+            BuildStabilizedTimelapse();
+            _builder.AddVignetting(intensity);
+        }
+    }
+}

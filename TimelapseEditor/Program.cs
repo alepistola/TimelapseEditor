9fe97688c3cd03e603d5a9f17b83f2df338976af
alepistola <pistola.alessandro@virgilio.it>
2020-05-15 02:11:50 +0200

    Added preset options

--- TimelapseEditor/Program.cs
+++ TimelapseEditor/Program.cs
@@ -12,10 +12,34 @@ namespace TimelapseEditor
         static void Main(string[] args)
         {
             string firstImagePath;
+            Dictionary<int, string> templateMap = new Dictionary<int, string>() { { 1, "FairyGreen.txt" },
+                                                                                  { 2, "WalPyne.txt"},
+                                                                                  { 3, "MountDramatic.txt"} };
 
             Welcome();
             int option = int.Parse(Console.ReadLine());
-            if (option == 2)
+            if (option == 3)
+            {
+                Console.WriteLine("which template do you want to apply? (specify the number) ");
+                foreach(KeyValuePair<int, string> temp in templateMap)
+                    Console.WriteLine($"{temp.Key} - {temp.Value}");
+                int template = int.Parse(Console.ReadLine());
+                if(template > 0 && templateMap.ContainsKey(template))
+                {
+                    Console.WriteLine("Please enter the full path of the first image");
+                    firstImagePath = Console.ReadLine();
+                    ITimelapseBuilder timelapseBuilder = new TimelapseBuilder(firstImagePath);
+                    Director director = new Director();
+                    director.SetBuilder(timelapseBuilder);
+                    director.BuildStabilizedWithPresetTimelapse(templateMap[template]);
+                    Console.WriteLine("[+] Operation terminated correctly!\n\nPress any key to exit");
+                    Console.ReadLine();
+                }
+                else
+                    Console.WriteLine("[x] Err: value not valid!");
+
+            }
+            else if (option == 2)
             {
                 Console.WriteLine("with what intensity value the vignette shall be? (1-5) ");
                 int intensity = int.Parse(Console.ReadLine());
@@ -31,9 +55,7 @@ namespace TimelapseEditor
                     Console.ReadLine();
                 }
                 else
-                {
                     Console.WriteLine("[x] Err: value not valid!");
-                }
             }
             else if (option == 1)
             {
@@ -47,24 +69,7 @@ namespace TimelapseEditor
                 Console.ReadLine();
             }
             else
-            {
                 Console.WriteLine("[x] Err: value not valid!");
-            }
-
-            /* Timelapse test */
-            //Console.WriteLine("Please enter the full filepath of the first image");
-            //firstImagePath = Console.ReadLine();
-            //Timelapse.Instance(firstImagePath);
-
-            //CameraRawAdapterProxy adapterProxy = new CameraRawAdapterProxy(firstImagePath);
-            //Console.WriteLine(adapterProxy.GetExif()["ExposureTime"]);
-
-            /* Analyze Exp test */
-            //Console.WriteLine("Please enter the full path of the first image");
-            //firstImagePath = Console.ReadLine();
-            //Timelapse.Instance(firstImagePath).AnalyzeExposure();
-
-
         }
 
         static void Welcome()
@@ -73,8 +78,9 @@ namespace TimelapseEditor
             Console.WriteLine("This programme will let you choose the type of changes you may");
             Console.WriteLine("want to apply at your raw footage timelapse. These are the options available:");
             Console.WriteLine("1. Smoothly ramp the exposure throughout a timelapse");
-            Console.WriteLine("2. Option 1 + the addition of a vignette with intensity from 1 to 5\n");
-            Console.WriteLine("Please enter the number of the modification you want to apply: ");
+            Console.WriteLine("2. Option 1 + the addition of a vignette with intensity from 1 to 5");
+            Console.WriteLine("3. Option 1 + the addition of a preset style (from a predefined list)\n");
+            Console.WriteLine("Please enter the number of the modification you want to perform: ");
         }
     }
 }
